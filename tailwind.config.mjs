0623674fbd4405ba79855b3b96d109c7af836b0c
Nacho Geoghegan <ignaciog00@gmail.com>
2024-05-08 21:50:07 -0300

    ♻️ Refactor MarkdownPostLayout and update tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 export default {
+	darkMode: 'class',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js',
 	],
 	theme: {
-		extend: {},
-	},
+		screens: {
+		  sm: '480px',
+		  md: '768px',
+		  lg: '976px',
+		  xl: '1440px',
+		},
+		colors: {
+		  gray: colors.coolGray,
+		  blue: colors.lightBlue,
+		  red: colors.rose,
+		  pink: colors.fuchsia,
+		},
+		fontFamily: {
+		  sans: ['Graphik', 'sans-serif'],
+		  serif: ['Merriweather', 'serif'],
+		},
+		extend: {
+		  spacing: {
+			'128': '32rem',
+			'144': '36rem',
+		  },
+		  borderRadius: {
+			'4xl': '2rem',
+		  }
+		}
+	  },
 	plugins: [
-		require('flowbite/plugin')
+		require('flowbite/plugin')({
+			charts: true,
+			forms: true,
+			tooltips: true,
+		})
 	],
 }
